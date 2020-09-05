11639d6d99af9fb10c8d924c23d8ebd122a5e5e9
theNcoder <nisalwickramage@gmail.com>
2020-09-05 09:53:06 +0800

    Added special charactor escapes.

--- lynda-download-organizer/index.js
+++ lynda-download-organizer/index.js
@@ -1,15 +1,17 @@
 var fs = require('fs');
 
+var sourceDir = 'C:\\nisal\\downloads\\software-testing-planning-tests-for-mobile';
+var pathSeperator = '\\';
+
 function moveFile(filename){
   var fileNameParts = filename.split('=>')[1].trim().split(' ');
   fileNameParts.splice(0,1);
   var filenameFilter = fileNameParts.join(' ');
 
-  var sourceDir = '/Users/nisal/Documents/tutorials/Node.js for C# Developers';
   fs.readdir(sourceDir, (err, files) => {
     var matchingFiles = files.filter(file => file.includes(filenameFilter))
     if (matchingFiles.length == 1) {
-      var source = sourceDir + '/' + matchingFiles[0];
+      var source = sourceDir + pathSeperator + matchingFiles[0];
       var destinationPath = getDestinationPath(filename);
       var destinationFilename = getDestinationFilename(filename);
       fs.mkdir(destinationPath, (err) => {
@@ -22,17 +24,17 @@ function moveFile(filename){
 }
 
 function getDestinationPath(filename){
-  var sourceDir = '/Users/nisal/Documents/tutorials/Node.js for C# Developers';
-  return sourceDir + '/' + filename.split('=>')[0].trim();
+  return sourceDir + pathSeperator + filename.split('=>')[0].trim();
 }
 
 function getDestinationFilename(filename){
-  var sourceDir = '/Users/nisal/Documents/tutorials/Node.js for C# Developers';
-  return sourceDir + '/' + filename.split('=>')[0].trim() + '/' + filename.split('=>')[1].trim() + '.mp4';
+  return sourceDir + pathSeperator + filename.split('=>')[0].trim() + pathSeperator + filename.split('=>')[1].trim() + '.mp4';
 }
 
 function organize(filename){
   filename = filename.replace('?', '');
+  filename = filename.replace(':', ' -');
+  filename = filename.replace('/', '_');
   moveFile(filename);
   // fs.rename(getSourcePath(filename), getDestinationPath(filename), () => console.log('copied'));
 }
